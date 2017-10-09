93162ab283e9e97214cc47ee12c00c82f1a503f6
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-09 17:57:55 +0200

    Added about-me component, with some scss and content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { SectionDisplayComponent } from './section-display/section-display.component';
 import { HomeComponent } from './section-display/home/home.component';
+import { AboutMeComponent } from './section-display/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     SectionDisplayComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
