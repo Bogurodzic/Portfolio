c06b80788375f0fd8378a0ae0e3229aa57173b4e
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-10 17:07:35 +0200

    Added skills to resume section

--- src/app/section-display/resume/resume.component.ts
+++ src/app/section-display/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery/dist/jquery';
 
 @Component({
   selector: 'app-resume',
@@ -10,7 +11,30 @@ export class ResumeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.changeSkillPercentage();
+
 
   }
+
+  changeSkillPercentage(){
+    let skills = document.querySelectorAll(".skill-item-container");
+
+    [].forEach.call(skills, skill => {
+      changeWidth(getSkillDiv(skill), getSkillPercentage(getSkillDiv(skill)))
+    });
+
+    function getSkillDiv(skillParent){
+      return skillParent.children[0];
+    }
+
+    function getSkillPercentage(skill){
+      return skill.dataset.percent;
+    }
+
+    function changeWidth(skill, percent){
+      skill.style.width = percent;
+    }
+  }
+
 
 }
