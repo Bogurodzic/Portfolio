81a588a3eb387b41a85a4f1ffb44f111116cd701
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-11 12:22:42 +0200

    Added click functions on categories

--- src/app/section-display/portfolio/portfolio.component.ts
+++ src/app/section-display/portfolio/portfolio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as velocity from 'velocity-animate/velocity';
+import Muuri from 'muuri/muuri';
+
+
 
 @Component({
   selector: 'app-portfolio',
@@ -10,6 +14,36 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // console.log(Muuri.ItemAnimate);
+    // var grid = new Muuri('.grid');
+    // console.log(grid.getItems())
+    // grid.filter('[data-foo]');
+
+    this.addEventsForCategories();
   }
 
+  addEventsForCategories(){
+    let categories = document.querySelectorAll(".portfolio-categories-list__item");
+
+    [].forEach.call(categories, category => {
+      console.log(category);
+
+      category.addEventListener("click", () => {
+        removeClassFromAll();
+        addClass(category);
+      });
+
+      function removeClassFromAll(){
+        [].forEach.call(categories, category => {
+          category.classList.remove("portfolio-categories-list__item--highlited");
+        })
+      }
+
+      function addClass(category){
+        category.classList.add("portfolio-categories-list__item--highlited");
+      }
+
+    });
+  }
+  
 }
