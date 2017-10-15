1a8a9ff6168f258d25ecc1dd485ea1d7da4bdcb5
Kamil Jarzab <jarzbkamil@gmail.com>
2017-10-15 14:50:36 +0200

    Added blog items

--- src/app/section-display/blog/blog.component.ts
+++ src/app/section-display/blog/blog.component.ts
@@ -8,8 +8,12 @@ import { BlogItem } from './blog-item/blog-item.model';
 })
 export class BlogComponent implements OnInit {
   posts: BlogItem[] = [
-    new BlogItem("assets/img/javascript.jpg", "Javascript", "Javascript sucks", "02", "May", "https://github.com/Bogurodzic"),
-    new BlogItem("assets/img/javascript.jpg", "Javascript", "Javascript sucks", "03", "May", "https://github.com/Bogurodzic")
+    new BlogItem("assets/img/javascript.jpg", "Javascript", "ES6 : WPROWADZENIE |CZ. 6| – KLASY", "29", "Sept", "http://kamiljarzab.pl/blog/2017/09/29/es6-wprowadzenie-cz-6-klasy/"),
+    new BlogItem("assets/img/javascript.jpg", "Javascript", "ES6 : WPROWADZENIE |CZ. 5| – DESTRUKTURYZACJA", "1", "Sept", "http://kamiljarzab.pl/blog/2017/09/01/es6-destrukturyzacja/"),
+    new BlogItem("assets/img/javascript.jpg", "Javascript", "ES6 : WPROWADZENIE |CZ. 4| – ARROW FUNCTIONS", "24", "Aug", "http://kamiljarzab.pl/blog/2017/08/24/es6-arrow-functions/"),
+    new BlogItem("assets/img/javascript.jpg", "Javascript", "ES6 : WPROWADZENIE |CZ. 3| – DOMYŚLNE WARTOŚCI PARAMETRÓW", "19", "May", "http://kamiljarzab.pl/blog/2017/05/19/es6-domyslne/"),
+    new BlogItem("assets/img/javascript.jpg", "Javascript", "ES6 : WPROWADZENIE |CZ. 2| – SPREAD & REST", "28", "April", "http://kamiljarzab.pl/blog/2017/04/28/wprowadzenie-do-es6-cz-2-spread-rest/"),
+    new BlogItem("assets/img/javascript.jpg", "Javascript", "ES6 : WPROWADZENIE |CZ. 1| – LET I CONST", "16", "April", "http://kamiljarzab.pl/blog/2017/04/16/es6-let-const/")
   ]
 
   constructor() { }
