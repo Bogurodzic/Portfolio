6ec5c897580042ddb5f4d324b0101ceabcff840e
Kamil Jarząb <jarzbkamil@gmail.com>
2020-11-25 18:53:27 +0100

    Removed skills added background to about me

--- src/app/section-display/about-me/about-me.component.ts
+++ src/app/section-display/about-me/about-me.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnInit, AfterViewInit {
+  public backgroundHeight: number;
 
   constructor() { }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.calculateBackground();
+    }, 500)
+  }
+
   ngOnInit() {
+ 
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.calculateBackground();
+  }
+
+  private calculateBackground(): void {
+    const display = document.getElementsByClassName('display')[0];
+    const displayHeight = display.clientHeight;
+
+    const headline = document.getElementsByClassName('headline')[0];
+    const headlineHeight = headline.clientHeight;
+
+    const bio = document.getElementsByClassName('bio')[0];
+    const bioHeight = bio.clientHeight;
+
+    const services = document.getElementsByClassName('services')[0];
+    const servicesHeight = services.clientHeight;
+
+    const backgroundHeight = displayHeight - headlineHeight - bioHeight - servicesHeight - 100;
+    console.log(displayHeight,headlineHeight,bioHeight,servicesHeight, backgroundHeight);
+
+    this.backgroundHeight = backgroundHeight;
   }
 
 }
