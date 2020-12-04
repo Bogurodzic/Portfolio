611a620aba0043a6aefaae8422be9fe67202859d
Kamil Jarzab <jarzbkamil@gmail.com>
2020-12-04 22:11:33 +0100

    Added compressed images

--- src/app/shared/data/projects.data.ts
+++ src/app/shared/data/projects.data.ts
@@ -4,88 +4,88 @@ export const projects: IProject[] = [
     {
         projectName: "Clik4Dom",
         projectType: "Mobile Application",
-        projectImages: ["assets/img/portfolio/gallery/clik4dom-1.png", "assets/img/portfolio/gallery/clik4dom-2.png"],
+        projectImages: ["assets/img/portfolio/gallery/clik4dom-1-min.png", "assets/img/portfolio/gallery/clik4dom-2-min.png"],
         categories: ["category-all", "category-mobile"],
         client: "Cravtt Property",
         siteUrl: "https://apps.apple.com/us/app/clik4dom/id1512735374?ign-mpt=uo%3D2",
         date: "2020.11.31",
         projectDescription: "The app dedicated for people, who want to sell or buy property via the Internet. It contains browsing of the offers, adding favourite ads, sending private messages and proposing meetings. The app provides calendar function and previewing meeting schedule. While creating, I was responsible for both back-end and front-end, and ensuring communication between them.",
         technologies: ["Node.js", "express.js", "mongoDB", "Ionic", "Angular", "Typescript", "Javascript", "ES6", "HTML5", "CSS3",  "SASS", "Android Studio", "xCode", "apidocjs"],
-        cover: "assets/img/portfolio/cover/clik4dom-cover.png"
+        cover: "assets/img/portfolio/cover/clik4dom-cover-min.png"
     },
     {
         projectName: "Zwyczajny.pl",
         projectType: "Mobile Application",
-        projectImages: ["assets/img/portfolio/gallery/zwyczajny-1.png"],
+        projectImages: ["assets/img/portfolio/gallery/zwyczajny-1-min.png"],
         categories: ["category-all", "category-mobile"],
         client: "Zwyczajny.pl",
         siteUrl: "http://zwyczajny.pl",
         date: "2018.06.01",
         projectDescription: "A social mobile application for arranging joint meals, where my responsibility As a front-end developer was to create new features, improving performance and asynch optimizations.",
         technologies: ["HTML5", "CSS3", "Javascript", "SASS", "Php", "MySql", "Wordpress", "Apache Cordova", "xCode", "Android Studio"],
-        cover: "assets/img/portfolio/cover/zwyczajny-cover.png"
+        cover: "assets/img/portfolio/cover/zwyczajny-cover-min.png"
     },
     {
         projectName: "Smart Agent - Reservis",
         projectType: "Mobile Application",
-        projectImages: ["assets/img/portfolio/gallery/smart-agent-1.png"], 
+        projectImages: ["assets/img/portfolio/gallery/smart-agent-1-min.png"], 
         categories: ["category-all", "category-mobile"],
         client: "Reservis",
         siteUrl: "https://play.google.com/store/apps/details?id=com.reservis&hl=pl",
         date: "2010.03.01",
         projectDescription: "Smart Agent is a mobile app for owners of the businesses based on the provision of services and who have an account in Reservis system. The app enables accepting reservations added through the Internet, via phone and personally, and setting employees' time. The app is very handy and enables 24/7 access to Calendar. I was responsible for front-end side of Reservis system, and ensuring communication with Web-Service.",
         technologies: ["Ionic", "Typescript", "HTML5", "CSS3", "Javascript", "Apache Cordova", "xCode", "Android Studio", "Firebase", "Websocket"],
-        cover: "assets/img/portfolio/cover/smart-agent-cover.png"
+        cover: "assets/img/portfolio/cover/smart-agent-cover-min.png"
     },
     {
         projectName: "Terminal",
         projectType: "Javascript Application",
-        projectImages: ["/assets/img/portfolio/gallery/terminal-1.png", "/assets/img/portfolio/gallery/terminal-2.png", "/assets/img/portfolio/gallery/terminal-3.png"],  
+        projectImages: ["/assets/img/portfolio/gallery/terminal-1-min.png", "/assets/img/portfolio/gallery/terminal-2-min.png", "/assets/img/portfolio/gallery/terminal-3-min.png"],  
         categories: ["category-all", "category-site"],
         client: "Reservis",
         siteUrl: "https://oferta.reservis.pl/",
         date: "2018.12.15",
         projectDescription: "Terminal is a system, which enables making a reservation online for business owners, and management of it. In that project I was responsible for building the front-end layer of the service, and enabling communication with back-end with REST api.",
         technologies: ["HTML5", "CSS3", "Javascript", "ES6", "jQuery", "Wordpress", "MySQL", "ImageMapster"],
-        cover: "assets/img/portfolio/cover/terminal-cover.png"
+        cover: "assets/img/portfolio/cover/terminal-cover-min.png"
     },
     {
         projectName: "Recepcja",
         projectType: "Javascript Application",
-        projectImages: ["/assets/img/portfolio/gallery/recepcja-1.png", "/assets/img/portfolio/gallery/recepcja-2.png", "/assets/img/portfolio/gallery/recepcja-3.png", "/assets/img/portfolio/gallery/recepcja-4.png", "/assets/img/portfolio/gallery/recepcja-5.png", "/assets/img/portfolio/gallery/recepcja-6.png", "/assets/img/portfolio/gallery/recepcja-7.png", "/assets/img/portfolio/gallery/recepcja-8.png"],  
+        projectImages: ["/assets/img/portfolio/gallery/recepcja-1-min.png", "/assets/img/portfolio/gallery/recepcja-2-min.png", "/assets/img/portfolio/gallery/recepcja-3-min.png", "/assets/img/portfolio/gallery/recepcja-4-min.png", "/assets/img/portfolio/gallery/recepcja-5-min.png", "/assets/img/portfolio/gallery/recepcja-6-min.png", "/assets/img/portfolio/gallery/recepcja-7-min.png", "/assets/img/portfolio/gallery/recepcja-8-min.png"],  
         categories: ["category-all", "category-site"],
         client: "Reservis",
         siteUrl: "https://oferta.reservis.pl/",
         date: "2018.12.15",
         projectDescription: "Recepcja enables clients to make reservation online in choosen place. In that project I was responsible for building the front-end layer of the service, and enabling communication with back-end with REST api.",
         technologies: ["HTML5", "CSS3", "Javascript", "ES6", "jQuery", "Wordpress", "MySQL", "ImageMapster"],
-        cover: "assets/img/portfolio/cover/recepcja-cover.png"
+        cover: "assets/img/portfolio/cover/recepcja-cover-min.png"
     },
     {
         projectName: "Ryzowa Park",
         projectType: "Javascript Application",
-        projectImages: ["/assets/img/portfolio/gallery/ryzowa-1.png", "/assets/img/portfolio/gallery/ryzowa-2.png", "/assets/img/portfolio/gallery/ryzowa-3.png", "/assets/img/portfolio/gallery/ryzowa-4.png"],  
+        projectImages: ["/assets/img/portfolio/gallery/ryzowa-1-min.png", "/assets/img/portfolio/gallery/ryzowa-2-min.png", "/assets/img/portfolio/gallery/ryzowa-3-min.png", "/assets/img/portfolio/gallery/ryzowa-4-min.png"],  
         categories: ["category-all", "category-site"],
         client: "Ryzowa Park",
         siteUrl: "http://ryzowapark.pl",
         date: "2018.12.15",
         projectDescription: "My responsibility was to create the apartment viewer app for existing Ryzowa Park site. In the app, thanks to visualisation, user could check the placement of the apartments and the actual condition, details about the choosen flat, including section and status. The app also offered possibility of apartment reservation and request to be contacted by the agent.",
         technologies: ["HTML5", "CSS3", "Javascript", "ES6", "jQuery", "Wordpress", "MySQL", "ImageMapster"],
-        cover: "assets/img/portfolio/cover/ryzowa-cover.png"
+        cover: "assets/img/portfolio/cover/ryzowa-cover-min.png"
     },
 
 
     {
         projectName: "Arcade Game",
         projectType: "Javascript",
-        projectImages: ["assets/img/portfolio/gallery/arcade-1.png"],
+        projectImages: ["assets/img/portfolio/gallery/arcade-1-min.png"],
         categories: ["category-all", "category-arcade", "category-site"],
         client: "Personal project",
         siteUrl: "http://kamil.webheroes.io/Projekty/Arcade/",
         date: "2016.10.11",
         projectDescription: "It is simple arcade game based on Frogger, it was made for Udacity Nanodegree Project made in Canvas.",
         technologies: ["HTML5", "CSS3", "Canvas", "Javascript"],
-        cover: "assets/img/portfolio/cover/arcade-cover.png"
+        cover: "assets/img/portfolio/cover/arcade-cover-min.png"
 
     },
   ];
